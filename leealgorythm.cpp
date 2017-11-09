06840ef870961bfc83bd0b4ce556eb7e68a2fab1
Ainur <hak.ain@yandex.ru>
2017-11-10 02:15:04 +0500

    разобрал алгоритм поиска кратчайшего пути из интернета и начал пилить свой

--- leealgorythm.cpp
+++ leealgorythm.cpp
@@ -1,123 +1,114 @@
-#include "leealgorythm.h"
-
-LeeAlgorythm::LeeAlgorythm()
-{
-
-}
-
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+const int H      = 8;          // ширина рабочего поля
+const int W      = 8;          // высота рабочего поля
+const int WALL   = 1;          // непроходимая ячейка
+const int BLANK  = 0;          // свободная непомеченная ячейка
 
-const int W      = 10;          // ширина рабочего поля
-const int H      = 10;         // высота рабочего поля
-const int WALL   = -1;         // непроходимая ячейка
-const int BLANK  = -2;         // свободная непомеченная ячейка
+struct Point {
+    int x;
+    int y;
+};
 
-int px[W * H], py[W * H];      // координаты ячеек, входящих в путь
+queue<Point> path;      // координаты ячеек, входящих в путь
 int len;                       // длина пути
-int grid[H][W];                // рабочее поле
+queue<Point> wave;
 
-// Перед вызовом lee() массив grid заполнен значениями WALL и BLANK
+int grid[W][H] = { { 0, 0, 1, 0, 0, 0, 0, 0 } ,
+                   { 0, 0, 1, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 1, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 1, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 0, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 0, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 0, 0, 1, 0, 0, 0 } ,
+                   { 0, 0, 0, 0, 1, 0, 0, 0 }
+                 };                // рабочее поле
 
-bool lee(int ax, int ay, int bx, int by)   // поиск пути из ячейки (ax, ay) в ячейку (bx, by)
+int hx[4] = {-1, +1,  0,  0 };
+int hy[4] = { 0,  0, -1, +1 };
+int mark = -1;
+
+int getGridVal(Point pos)
 {
-    int dx[4] = {1, 0, -1, 0};   // смещения, соответствующие соседям ячейки
-    int dy[4] = {0, 1, 0, -1};   // справа, снизу, слева и сверху
-    int d, x, y, k;
-    bool stop;
+    return grid[pos.x][pos.y];
+}
 
-    if (grid[ay][ax] == WALL || grid[by][bx] == WALL)
-        return false;  // ячейка (ax, ay) или (bx, by) - стена
+void setGridVal(Point pos, int val)
+{
+    grid[pos.x][pos.y] = val;
+}
 
-    // распространение волны
-    d = 0;
-    grid[ay][ax] = 0;            // стартовая ячейка помечена 0
-    do {
-        stop = true;               // предполагаем, что все свободные клетки уже помечены
-        for ( y = 0; y < H; ++y )
-            for ( x = 0; x < W; ++x )
-                if ( grid[y][x] == d )                         // ячейка (x, y) помечена числом d
-                {
-                    for ( k = 0; k < 4; ++k )                    // проходим по всем непомеченным соседям
-                    {
-                        int iy=y + dy[k], ix = x + dx[k];
-                        if ( iy >= 0 && iy < H && ix >= 0 && ix < W && grid[iy][ix] == BLANK )
-                        {
-                            stop = false;              // найдены непомеченные клетки
-                            grid[iy][ix] = d + 1;      // распространяем волну
-                        }
-                    }
-                }
-        d++;
-    } while ( !stop && grid[by][bx] == BLANK );
 
-    if (grid[by][bx] == BLANK)
-        return false;  // путь не найден
-
-    // восстановление пути
-    len = grid[by][bx];            // длина кратчайшего пути из (ax, ay) в (bx, by)
-    x = bx;
-    y = by;
-    d = len;
-    while ( d > 0 )
+Point littleWave( Point pos )
+{
+    for(int i = 0; i < 4; ++i)
     {
-        px[d] = x;
-        py[d] = y;                   // записываем ячейку (x, y) в путь
-        d--;
-        for (k = 0; k < 4; ++k)
+        Point tmpPoint = { pos.x + hx[i], pos.y + hy[i] };
+        if(getGridVal(tmpPoint) == 0 &&
+                pos.x + hx[i] >= 0 &&
+                pos.x + hy[i] < W &&
+                pos.y + hy[i] >= 0 &&
+                pos.y + hy[i] < H)
         {
-            int iy=y + dy[k], ix = x + dx[k];
-            if ( iy >= 0 && iy < H && ix >= 0 && ix < W && grid[iy][ix] == d)
-            {
-                x = x + dx[k];
-                y = y + dy[k];           // переходим в ячейку, которая на 1 ближе к старту
-                break;
-            }
+            wave.push(tmpPoint);
+            setGridVal(tmpPoint, getGridVal(pos) - 1);
         }
     }
-    px[0] = ax;
-    py[0] = ay;                    // теперь px[0..len] и py[0..len] - координаты ячеек пути
-    return true;
 }
-#if 0
-int main(int argc, char *argv[])
+
+bool CustomLee(Point beginPoint, Point endPoint)
 {
-    for(size_t i = 0; i < W; ++i)
-        for(size_t j = 0; j < H; ++j)
-            grid[i][j] = -2;
+    if(getGridVal(beginPoint) || getGridVal(endPoint))
+        return false;
+
+    wave.push(beginPoint);
+
+
+    setGridVal(beginPoint, mark);
+
+    cout << (wave.front()).x << endl;
+    cout << (wave.front()).y << endl;
+
+    bool stop = true;
+    while(stop)
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            Point tmpPoint = { (wave.front()).x + hx[i], (wave.front()).y + hy[i] };
+            if(tmpPoint.x == endPoint.x && tmpPoint.y == endPoint.y)
+                stop = false;
+            if(getGridVal(tmpPoint) == 0 &&
+                    (wave.front()).x + hx[i] >= 0 &&
+                    (wave.front()).x + hy[i] < W &&
+                    (wave.front()).y + hy[i] >= 0 &&
+                    (wave.front()).y + hy[i] < H)
+            {
+                wave.push(tmpPoint);
+                //wave.pop();
+                setGridVal(tmpPoint, getGridVal((wave.front())) - 1);
+                stop = true;
+            }
+        }
+        cout << wave.empty() << endl;
+    }
+    cout << "CustomLee" << endl;
+}
+
+
+int main()
+{
+    Point p1 = {0, 0};
+    Point p2 = {2, 3};
+    CustomLee(p1, p2);
 
     for(size_t i = 0; i < W; ++i) {
         for(size_t j = 0; j < H; ++j)
             cout << grid[i][j] << " ";
         cout << endl;
     }
-
-    for(size_t i = 0; i < W * 5; ++i)
-        cout << px[i] << " ";
-    cout << endl;
-
-    for(size_t i = 0; i < W * 5; ++i)
-        cout << py[i] << " ";
-    cout << endl;
-
-    lee(1, 1, 5, 4);
-
-    for(size_t i = 0; i < W; ++i) {
-        for(size_t j = 0; j < H; ++j)
-            cout << grid[i][j] << " ";
-        cout << endl;
-    }
-
-    for(size_t i = 0; i < W * 5; ++i)
-        cout << px[i] << " ";
-    cout << endl;
-
-    for(size_t i = 0; i < W * 5; ++i)
-        cout << py[i] << " ";
-    cout << endl;
 
     return 0;
 }
-#endif
