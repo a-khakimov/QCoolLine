3cd9c8235fa7ea221679e868dc7ede9b78eb4a7e
Ainur <hak.ain@yandex.ru>
2017-11-12 03:25:56 +0500

    удалил лишний код

--- main.cpp
+++ main.cpp
@@ -5,25 +5,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-/*
-    MainWindow widget;
-    widget.show();
-   */
-
-    QVector<int> v1;
-    v1.resize(10);
-    v1[0] = 9;
-    v1[1] = 3;
-    v1[2] = 5;
-    v1[3] = 13;
-
-    QVector<int> v2;
-    v2.resize(5);
-
-    v2 = v1;
-    qDebug() << v1 << v2;
-    v1[0] = 1;
-    qDebug() << v1 << v2;
 
     QCoolLine coolline;
 
